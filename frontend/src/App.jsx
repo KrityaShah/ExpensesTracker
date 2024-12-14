8e5eb2f9e1a39a4c402e28d6d5c608c29614f787
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-12-14 11:40:47 +0545

    Signup Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 function App() {
  
 
   return (
     <>
-      <Login/>
+    <Signup/>
+      {/* <Login/> */}
     </>
   )
 }
