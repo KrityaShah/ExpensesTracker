e263053be30fa98b81178f8ec390014d1b2145fd
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-12-14 14:31:54 +0545

    updated controller
    bcrypt
    jwt

--- backend/controller/auth-controllers.js
+++ backend/controller/auth-controllers.js
@@ -1,22 +1,43 @@
-const home = async (req, res) =>{
-    try {
-        res.status(200).json( {message: "Hello world via controller"})
-    } catch (error) {
-        console.error(error);
-        
+const User = require("../model/user-model");
+const bcrypt = require("bcryptjs");
+
+const home = async (req, res) => {
+  try {
+    res.status(200).json({ message: "Hello world via controller" });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const register = async (req, res) => {
+  try {
+    const { username, email, phone, password } = req.body;
+
+    const userExist = await User.findOne({ email });
+
+    if (userExist) {
+      return res.status(400).json({ message: "User Already Exists!" });
     }
-}
+    const saltRound = 10;
+    const hashPassword = await bcrypt.hash(password, saltRound);
 
+    const userCreated = await User.create({
+      username,
+      email,
+      phone,
+      password: hashPassword,
+    });
 
-const register = async (req, res) =>{
-    try {
-        res.status(200).json( {message: "register via controller"})
-        
-    } catch (error) {
-        console.error(error);
-        
-    }
-}
+    res
+      .status(200)
+      .json({
+        message: "Registration Sucessfull",
+        token: await userCreated.generateToken(),
+        userId: userCreated._id.toString(),
+      });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-
-module.exports = {home, register}
+module.exports = { home, register };
--- backend/router/auth-router.js
+++ backend/router/auth-router.js
@@ -5,6 +5,6 @@ const authControllers = require('../controller/auth-controllers')
 
 
 router.route('/').get(authControllers.home)
-router.route('/register').get(authControllers.register)
+router.route('/register').post(authControllers.register)
 
 module.exports = router;
