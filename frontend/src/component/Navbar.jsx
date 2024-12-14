cc981c0a01fd8689c712a375d3869a808fe08e03
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-12-14 22:58:22 +0545

    Updated Navbar

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
-import "./Navbar.css"
+import React from "react";
+import "./Navbar.css";
 import { Link } from "react-router-dom";
-
+import { useAuth } from "../store/auth";
 
 const Navbar = () => {
-  return (
-    <div className='Main-NavContainer'>
-        <div className="head-nav">
-            <h1>Expenses Tracker</h1>
-        </div>
-      <ul>
-       <Link to="/" className="nav-link"><li>Login</li></Link> 
-      <Link to="/signup" className="nav-link"><li>Sign up</li></Link> 
-      </ul>
-    </div>
-  )
-}
+  const { isLoggedIn } = useAuth();
 
-export default Navbar
+  return (
+    <div className="Main-NavContainer">
+      <div className="head-nav">
+        <h1>ITracker</h1>
+      </div>
+      {isLoggedIn ? (
+        <>
+          <ul>
+            <Link to="/dashboard" className="nav-link">
+              <li>home</li>
+            </Link>
+            <Link to="/profile" className="nav-link">
+              <li>profile</li>
+            </Link>
+            <Link to="" className="nav-link">
+              <li>Expenses</li>
+            </Link>
+            <Link to="" className="nav-link">
+              <li>Logout</li>
+            </Link>
+          </ul>
+        </>
+      ) : (
+        <>
+          <ul>
+            <Link to="/" className="nav-link">
+              <li>Login</li>
+            </Link>
+            <Link to="/signup" className="nav-link">
+              <li>Sign up</li>
+            </Link>
+          </ul>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
